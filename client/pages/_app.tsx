bf47a9f0ac331b853483d4c51201835a06e23c94
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-08-30 13:17:59 +0300

    feat: add typescript to client

---
+++ client/pages/_app.tsx
@@ -0,0 +1,5 @@
+import "../styles/globals.css";
+
+const MyApp = ({ Component, pageProps }: any) => <Component {...pageProps} />;
+
+export default MyApp;
---
+++ client/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Home = () => (
+    <div>
+        <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+            <h1>next app</h1>
+        </main>
+
+        <footer></footer>
+    </div>
+);
+
+export default Home;
