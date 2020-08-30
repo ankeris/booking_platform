82abc390a2067058d83fc3736bc9814598b836a1
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-08-30 13:50:58 +0300

    feat(prettier): add config file

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,17 @@
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }: any) => <Component {...pageProps} />;
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "/admin/api"
+  })
+});
+
+const MyApp = ({ Component, pageProps }: any) => (
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+);
 
 export default MyApp;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,33 @@
 import Head from "next/head";
+import { useQuery, gql } from "@apollo/client";
 
-const Home = () => (
+const ALL_TODOS = gql`
+  query {
+    allTodos {
+      name
+    }
+  }
+`;
+const Home = () => {
+  const { loading, error, data } = useQuery(ALL_TODOS);
+
+  if (error) return <h1>{error.message}</h1>;
+  return loading ? (
+    <h1>loading</h1>
+  ) : (
     <div>
-        <Head>
-            <title>Create Next App</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main>
-            <h1>next app</h1>
-        </main>
+      <main>
+        <h1>next app</h1>
+      </main>
 
-        <footer></footer>
+      <footer></footer>
     </div>
-);
+  );
+};
 
 export default Home;
