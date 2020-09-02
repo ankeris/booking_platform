57907cf45246952a11e01ed31e48ad55b8995735
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-09-02 19:39:27 +0300

    client update

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,40 +1,49 @@
 import Head from "next/head";
 import { useQuery, gql } from "@apollo/client";
+import dynamic from "next/dynamic";
 
-const ALL_TODOS = gql`
-  query {
-    allTodos {
-      name
-      description
+const TechnologiesFactory = dynamic(() => import("@components/TechnologiesFactory"), { ssr: false });
+
+const ALL_PROJECTS = gql`
+    query {
+        allProjects {
+            title
+            description
+            previewImage {
+                publicUrl
+            }
+        }
     }
-  }
 `;
+
 const Home = () => {
-  const { loading, error, data } = useQuery(ALL_TODOS);
+    const { loading, error, data } = useQuery(ALL_PROJECTS);
 
-  if (error) return <h1>{error.message}</h1>;
-  return loading ? (
-    <h1>loading</h1>
-  ) : (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>booking app </h1>
-      <main>
-        <ul>
-          {data.allTodos.map(x => (
-            <li>
-              {x.name}: {x.description}
-            </li>
-          ))}
-        </ul>
-      </main>
+    if (error) return <h1>{error.message}</h1>;
+    return loading ? (
+        <h1>loading</h1>
+    ) : (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Juozas Rastenis Portfolio</h1>
+            <main>
+                <TechnologiesFactory></TechnologiesFactory>
+                <ul>
+                    {data.allProjects.map(x => (
+                        <li key={x.title}>
+                            {x.title}: {x.description}
+                            <img src={x.previewImage?.publicUrl} alt="lyra" width="150" />
+                        </li>
+                    ))}
+                </ul>
+            </main>
 
-      <footer></footer>
-    </div>
-  );
+            <footer></footer>
+        </div>
+    );
 };
 
 export default Home;
